c7e55bdfe9a84b128968178e2584a5267d5425c7
stockmaj <stockmaj@users.noreply.github.com>
2014-02-24 08:25:38 -0500

    Update TimingBehavior.java

--- src/main/java/com/nitorcreations/metrics/wicket/TimingBehavior.java
+++ src/main/java/com/nitorcreations/metrics/wicket/TimingBehavior.java
@@ -4,9 +4,10 @@ import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.html.WebPage;
 
-import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.Timer;
-import com.yammer.metrics.core.TimerContext;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import com.codahale.metrics.Timer.Context;
+
 
 /**
  * Adds a metric to the {@link WebPage} that this {@link Behavior} is attached to. This metric will
@@ -18,13 +19,13 @@ import com.yammer.metrics.core.TimerContext;
 public class TimingBehavior extends Behavior {
     private static final long serialVersionUID = -1087866639896000332L;
     private transient Timer timer;
-    private transient TimerContext timerContext;
+    private transient Context timerContext;
 
     private Class<?> componentClazz;
 
     private Timer getTimer() {
         if (timer == null) {
-            timer = Metrics.newTimer(componentClazz, "Page render time");
+            timer = new MetricRegistryRom().getObject().timer(MetricRegistry.name(componentClazz, "Page render time"));
         }
         return timer;
     }
